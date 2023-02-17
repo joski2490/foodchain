7f2b011f71b29bf3458a1b8083613640ed32f340
joski2490 <68449917+joski2490@users.noreply.github.com>
2023-02-17 13:19:07 -0600

    Update entity.js

--- scripts/entity.js
+++ scripts/entity.js
@@ -52,7 +52,7 @@ class Entity {
 
         // Do not draw entity on line mode
         if (!lineMode) {
-            ellipse(this.pos.x, this.pos.y, this.radius * 2, this.radius * 2);
+            ellipse(this.pos.x, this.pos.y, this.radius * 1, this.radius * 1);
         }
     }
 
@@ -71,7 +71,7 @@ class Entity {
     }
 
     getNearest(entities) {
-        var lowestDist = 10000;
+        var lowestDist = 1000000;
         var e = entities[0];
         for (var i = 0; i < entities.length; i++) {
             var dist = entities[i].pos.dist(this.pos);
