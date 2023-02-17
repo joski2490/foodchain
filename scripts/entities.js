7cc26aca9f197d34047a6e4943599f16fb0c5b65
joski2490 <68449917+joski2490@users.noreply.github.com>
2023-02-17 11:23:35 -0600

    Update entities.js

--- scripts/entities.js
+++ scripts/entities.js
@@ -215,7 +215,7 @@ templates.prey = {
     toChase: ['food'],
     toEat: ['food'],
     topSpeed: 6,
-    tVel: 0.1,
+    tVel: 12,
     onEat: function(e, newEntities) {
         if (this.eat(e)) {
             var x = this.pos.x + random(-20, 20);
